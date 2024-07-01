f8dd1867ddef169318b2adc15b24f7a04245025c
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-07-01 06:25:59 +0530

    Create 443. String Compression.java

---
+++ 443. String Compression.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int compress(char[] chars) {
+        int count = 1 ;
+        StringBuilder sb = new StringBuilder("");
+
+        sb.append(chars[0]);
+
+        for(int i = 1 ;i<chars.length;i++){
+
+            if(chars[i-1]!=chars[i]){
+                if(count>1){
+                    sb.append(count+"");
+                }
+                sb.append(chars[i]);
+                count = 1;
+
+            }else{
+                count++;
+            }
+        }
+
+        if(count>1){
+            sb.append(count+"");
+        }
+
+        for(int i=0;i<sb.length();i++){
+            char ch = sb.charAt(i);
+            chars[i]=ch;
+        }
+
+        return sb.length();
+
+
+    }
+}
