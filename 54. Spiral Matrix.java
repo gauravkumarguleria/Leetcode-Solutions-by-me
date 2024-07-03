770f3b0177f5ee14b2e93ec3e5a9fe59e922149d
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-07-03 12:18:26 +0530

    Create 54. Spiral Matrix.java

---
+++ 54. Spiral Matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] mat) {
+     List<Integer> spiral = new ArrayList<>();
+      int fr=0, fc=0, lr=mat.length-1, lc=mat[0].length-1;
+
+      while(true){
+        //top wall(left to right)
+        for(int c =fc; c<=lc;c++){
+            spiral.add(mat[fr][c]);
+        }
+        fr++;
+        if(fr>lr) break;
+        
+        //last wall(top to bottom)
+        for(int r=fr;r<=lr;r++){
+            spiral.add(mat[r][lc]);
+        }
+        lc--;
+        if(fc>lc) break;
+
+        //bottom wall(right to left)
+
+        for(int c=lc;c>=fc;c--){
+            spiral.add(mat[lr][c]);
+        }
+        lr--;
+        if(fr>lr) break;
+
+        //left wall(bottom to top)
+        for(int r=lr; r>=fr; r--){
+            spiral.add(mat[r][fc]);
+        }
+        fc++;
+        if(fc>lc) break;
+        
+        } 
+
+        return spiral; 
+    }
+}
