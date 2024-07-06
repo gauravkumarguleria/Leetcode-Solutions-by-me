d9d1a981700087a02588ac06cf6ad1a03b59e71d
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-07-06 08:47:44 +0530

    Create 1011. Capacity To Ship Packages Within D Days.java

---
+++ 1011. Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int shipWithinDays(int[] books, int studs) {
+        long left = max(books) , right= sum(books);
+        while(left<=right){
+            long mid = (left+right)/2;
+            if(isPossible(books, studs,mid) == true)
+                right = mid-1;
+            else left = mid+1;
+        }   
+        return (int)left;
+    }
+
+
+    public long max(int[] books){
+        long ans = 0;
+        for(long pages : books){
+            ans = Math.max(ans,pages);
+        }
+        return ans;
+    }
+
+     public long sum(int[] books){
+        long ans = 0;
+        for(long pages : books){
+            ans += pages;
+        }
+        return ans;
+    }
+
+    public boolean isPossible(int[] books, int allowedStuds, long maxLoad){
+        long currLoad = 0;
+        int reqdStuds = 1;
+
+        for(long pages: books){
+            if(currLoad + pages <= maxLoad)
+                currLoad+=pages;
+        
+        else{
+            currLoad = pages;
+            reqdStuds++;
+        }
+        }
+ return (reqdStuds<=allowedStuds);
+    }
+}
