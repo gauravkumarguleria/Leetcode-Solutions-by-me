d0e8a45d0e71ea8c5a48c4bc860b869eb0f4e957
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-07-14 10:38:54 +0530

    Create 148. Sort List.java

---
+++ 148. Sort List.java
@@ -0,0 +1,52 @@
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMiddle(head);
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left, right);
+    }
+
+    private ListNode getMiddle(ListNode head) {
+        if (head == null) return head;
+
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null) {
+            current.next = l1;
+        } else if (l2 != null) {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
