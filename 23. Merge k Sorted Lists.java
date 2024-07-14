2a90d9c0d2e638a4c78f98cc05c73e8b686f46c9
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-07-14 10:37:52 +0530

    Create 23. Merge k Sorted Lists.java

---
+++ 23. Merge k Sorted Lists.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        while (lists.length > 1) {
+            List<ListNode> mergedLists = new ArrayList<>();
+            for (int i = 0; i < lists.length; i += 2) {
+                ListNode l1 = lists[i];
+                ListNode l2 = (i + 1 < lists.length) ? lists[i + 1] : null;
+                mergedLists.add(mergeTwoLists(l1, l2));
+            }
+            lists = mergedLists.toArray(new ListNode[0]);
+        }
+
+        return lists[0];
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = (l1 != null) ? l1 : l2;
+
+        return dummyHead.next;
+    }
+}
