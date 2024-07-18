62f103f456ee915a43684d6356d003c9cbb37b5b
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-07-18 20:06:47 +0530

    Create 503. Next Greater Element II.java

---
+++ 503. Next Greater Element II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] nextGreaterElements(int[] arr) {
+        int n = arr.length;
+        int [] nge = new int[n];
+        Arrays.fill(nge,-1);
+        Stack<Integer> stk = new Stack<>();
+
+        for(int idx = 0; idx<n ; idx++){
+            while(stk.size()>0 && arr[idx]> arr[stk.peek()]){
+            nge[stk.pop()]=arr[idx];
+        }
+        stk.push(idx);
+        }
+    for(int idx=0;idx<n ; idx++){
+        while(stk.size()>0 && arr[idx]>arr[stk.peek()])
+        nge[stk.pop()] = arr[idx];
+    }
+    return nge;
+    }
+
+}
