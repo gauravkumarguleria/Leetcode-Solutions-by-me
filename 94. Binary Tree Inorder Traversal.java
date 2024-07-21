ec1cd07f5ec4327e201a130c530e95a58dfb2108
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-07-21 08:22:10 +0530

    Create 94. Binary Tree Inorder Traversal.java

---
+++ 94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,13 @@
+class Solution {
+    List<Integer>inorder = new ArrayList<>();
+    public void dfs(TreeNode root){
+        if(root==null) return;
+        dfs(root.left);
+        inorder.add(root.val);
+        dfs(root.right);
+    }
+  public List<Integer> inorderTraversal(TreeNode root) {
+    dfs(root);
+    return inorder;
+  }
+}
