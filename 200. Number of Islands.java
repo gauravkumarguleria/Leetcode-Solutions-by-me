e6d7e25649983bbde0ab6fc81b3481b6dae1cd4d
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-03 07:19:55 +0530

    Create 200. Number of Islands.java

---
+++ 200. Number of Islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    int [] dr = {-1,+1,0,0};
+    int [] dc = {0,0,-1,+1};
+    public void dfs(char[][] grid, int r, int c){
+        if(r<0||c<0||r>=grid.length||c>=grid[0].length)
+            return;
+        if(grid[r][c] == '0' || grid[r][c] == '2')
+            return;
+        grid[r][c] = '2';
+        
+        for(int i=0; i< 4; i++)
+            dfs(grid,r+dr[i],c+dc[i]);
+        
+    }
+    public int numIslands(char[][] grid) {
+        int islands =0;
+        for(int r=0; r<grid.length; r++){
+            for(int c=0; c<grid[0].length; c++){
+                if(grid[r][c] == '1')
+                    islands++;
+                    dfs(grid, r, c);
+            }
+        }
+        return islands;
+    }
+}
