61eb8d0153e45220a8f7260ea6ae21261dcf239e
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-03 07:17:34 +0530

    Create 1971. Find if Path Exists in Graph.java

---
+++ 1971. Find if Path Exists in Graph.java
@@ -0,0 +1,39 @@
+class Graph{
+    public int n;
+    public ArrayList<Integer>[] adj;
+
+    public Graph(int n){
+        this.n = n;
+        adj = new ArrayList[n];
+        for(int i=0; i <n; i++)
+            adj[i] = new ArrayList<>();
+    }
+
+    public void addEdge(int src, int dest){
+        adj[src].add(dest);
+        adj[dest].add(src);
+    }
+
+    public boolean hasPath(int src, int dest, boolean[] vis){
+        if(src == dest) return true;
+        if(vis[src] == true) return false;
+
+        vis[src] = true;
+        for(int nbr: adj[src]){
+            if(hasPath(nbr, dest, vis) == true){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        Graph g = new Graph(n);
+        for(int[] edge : edges){
+            g.addEdge(edge[0],edge[1]);
+        }
+        boolean[] vis = new boolean[n];
+        return g.hasPath(source, destination, vis);
+    }
+}
