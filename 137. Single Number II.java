a8cb6cc2bcd5c174ace84f34358bb54478340004
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-08 17:20:39 +0530

    Create 137. Single Number II.java

---
+++ 137. Single Number II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int bit = 0; bit<32; bit++){
+            int mask = (1<<bit);
+            int counter=0;
+
+            for(int val : nums){
+                if((val&mask)!=0)
+                    counter++;
+            }
+
+            if(counter%3 != 0)
+                res = res|mask;
+        }
+        return res;
+    }
+}
