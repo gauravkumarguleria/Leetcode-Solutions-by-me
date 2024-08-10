91cef06fcd98f77c11d5f6c51fddaa47395d226a
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-10 08:21:26 +0530

    Create 1442. Count Triplets That Can Form Two Arrays of Equal XOR.java

---
+++ 1442. Count Triplets That Can Form Two Arrays of Equal XOR.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countTriplets(int[] arr) {
+        int ans = 0;
+        for(int l =0; l<arr.length; l++){
+            int xor = 0;
+            for(int r =l; r<arr.length; r++){
+                xor = xor^arr[r];
+                if(xor == 0) ans+=(r-l);
+            }
+        }
+        return ans;
+    }
+}
