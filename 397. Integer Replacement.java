74bf939f96036cd7d1db455651ea5e4481c60ec8
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-10 08:25:14 +0530

    Create 397. Integer Replacement.java

---
+++ 397. Integer Replacement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int integerReplacement(int n) {
+        if(n == Integer.MAX_VALUE) return 32;
+        if(n<=3) return n-1;
+        if(n%2 == 0) return 1 + integerReplacement(n/2);
+
+        if(((n-1)/2)%2 == 0)
+            return 1 + integerReplacement(n-1);
+        else return 1 + integerReplacement(n+1);
+    }
+}
