141ddf4e338053f11aa796885b3098aece9a31eb
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-22 06:23:55 +0530

    Create 377. Combination Sum IV.java

---
+++ 377. Combination Sum IV.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int combinationSum4(int[] coins, int amount) {
+        int [] dp = new int[amount + 1];
+        dp[0]=1;
+        for(int i =1; i<=amount; i++){
+            for(int coin: coins)
+                if(i>=coin){
+                    dp[i] += dp[i-coin];
+                }
+        }
+        return dp[amount];
+    }
+}
