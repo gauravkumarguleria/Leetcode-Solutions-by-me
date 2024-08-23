7695ce10a67d7923beea22081ca7ca39d573fb5e
Gaurav Kumar Guleria <523410004@nitkkr.ac.in>
2024-08-23 11:05:18 +0530

    Create 309. Best Time to Buy and Sell Stock with Cooldown.java

---
+++ 309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length<=1) return 0;
+
+        int [] buy = new int[prices.length];
+        int [] sell = new int[prices.length];
+        
+        buy[0]= -prices[0];
+        sell[0] = 0;
+        buy[1] = Math.max(-prices[0], -prices[1]);
+        sell[1] = Math.max(0, prices[1]-prices[0]);
+
+        for(int i=2;i< prices.length; i++){
+            buy[i] = Math.max(buy[i-1], sell[i-2] - prices[i]);
+            sell[i] = Math.max(sell[i-1], buy[i-1] + prices[i]);
+        }
+        return sell[prices.length - 1];
+    }
+}
